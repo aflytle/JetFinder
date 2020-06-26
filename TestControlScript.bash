f427d4be387c03ea9544661d569094511434df4a
aflytle <aflytle@uncg.edu>
2020-06-26 14:09:23 -0400

    FIXED GOOD STUFF

--- TestControlScript.bash
+++ TestControlScript.bash
@@ -24,24 +24,32 @@ then
 fi
 filename="testfile"
 
-file="TEST_FILES/"
+file="TEST_FILES"
 #add line to check for arguments
 
 
 
 
-if [ ! -f  $newdir$filename$filetype ]
-then
-   ../Ripser/ripser/ripser $file/*.txt --format point-cloud --dim 0 > $newdir$filename$filetype
-fi
+#if [ ! -f  $newdir$filename$filetype ]
+#then
+num=0
+for i in $file/*
+do
+    let num++
+    nums="_"$num"_"
+    ../Ripser/ripser/ripser $i --format point-cloud --dim 0 > $newdir$filename$nums$filetype
+    thesevals= python3 five_point_puller.py $E $newdir$filename$nums$filetype
+done
+#fi
 
 
 
 
 #pull top five lifetimes from that collison
-#thesevals= python3 five_point_puller.py $E $newdir$filename$filetype
+thesevals= python3 five_point_puller.py $E $newdir$filename$nums$filetype
+
 
 #compare the means from the test runs with the values from the five_point_puller script
-#result= python3 Fuzzy_Boolean.py $meanvals $thesevals
+result= python3 Fuzzy_Boolean.py $meanvals $thesevals
 
-#echo $result
+echo $result
